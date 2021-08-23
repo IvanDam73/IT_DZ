52de3db987701e5213abaff01621becb408b89c9
IvanDam73 <65127947+IvanDam73@users.noreply.github.com>
2021-08-23 16:11:12 +0300

    Add files via upload

--- MOOD/script.js
+++ MOOD/script.js
@@ -1,92 +1,85 @@
 "use strict"
 
 function HashStorageFunc() {
-    var myHash = {};
+    this.myHash = {};
 
     this.addValue = function(key, value){
-        return (myHash[key]=value);
+        this.myHash[key]=value;
     };
 
     this.getValue = function(key){
-        if(myHash[key]){
-            return myHash[key];
-        }else{
-            return undefined;
-        }
+        return this.myHash[key];
     };
 
     this.deleteValue = function(key){
-        if(!myHash[key]){
-            return false;
-        }else{
-            delete myHash.key;
-        }
+        return delete this.myHash[key];
     };
 
     this.getKeys = function(){
-        return Object.keys(myHash);
+        return Object.keys(this.myHash);
     };
     
-    return myHash;
 } 
 
 var drinkStorage = new HashStorageFunc();
 
 
-function enterInfo(){         //функция-хранилище, которая сохраняет введённые данные, и отдает другим функциям-кнопкам
-var g = {};
-
+function enterInfo(){         
 do{
     var first  = prompt('Введите название напитка', '');
 }while(!first);
 
-g['Напиток'] = first;
+var second =    confirm('Алкогольный он?');
 
-do{
-    var second =    prompt('Алкогольный он?', '');
-}while(!second);
-
-g['Алкоголь'] = second;
+if(second === true){
+    second = 'Да';
+}else{
+    second = 'Нет';
+}
 
 do{
     var third = prompt('Как он готовиться?', '');
 }while(!third);
 
-g['Рецепт приготовления'] = third;
- 
-console.log(g);
+drinkStorage.addValue(first, {alcohol:second, recipe:third});
+
 }
 
 
 
 
-function drinkInfo(){   //эта функция принимает название напитка и сравнивает его с имеющимся в хранилище(первая функция)
+function drinkInfo(){   
 do{
     var info = prompt('Введите название напитка', '');
 }while(!info);
-    if(info in g){
-        alert(drinkStorage.getKeys);
-    }else{
-        alert('Такого напитка нет!');
-    }
-    return;
+
+drinkStorage.getValue(info);
+var prop1  = drinkStorage.myHash[info].recipe;   
+var prop2  = drinkStorage.myHash[info].alcohol;
+
+if(info in drinkStorage.myHash){
+    alert(`    Напиток: ${info}
+    Алкогольный: ${prop2}
+    Рецепт приготовления: ${prop1}`);      
+}else{                                          //ошибка при else!   Cannot read property 'recipe' of undefined
+    alert('такого напитка нет!');
+}
 }
 
 
-function deleteInfo(){    //эта удаляет по той же схеме, что и придыдущая ищет
+function deleteInfo(){
 do{
     var del = prompt('Какой напиток удалить?', '');
 }while(!del);
-    if(del in g){   
-        delete g[del];
-        alert('Напиток удалён!');
-    }else{
-        alert('Напиток не найден!');
-    }
-    return;
+
+if(del in drinkStorage.myHash){
+    drinkStorage.deleteValue(del);
+    alert('Напиток удалён!');
+}
+    console.log(drinkStorage);
 }
 
 
-function allDrinks(){   //эта отдает массив ключей хранилища
-    return Object.keys(g);
+function allDrinks(){ 
+     alert(Object.keys(drinkStorage.myHash));
 }
